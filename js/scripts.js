e7b1ede0f3b91cfd3749c8065158de9f490d833e
pete-ewg17 <peteakhurst-ewg@Petes-MacBook-Pro.local>
2017-05-24 18:02:45 +1000

    fixed ig size

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@ $(function() {
                 userId: '46534099',
                 accessToken: '46534099.1f02994.3995f4253107449788ace4100057df87',
                 template: "<div class='insta-col col-xs-6'><a target='_blank' href='{{link}}'><img src='{{image}}'/></a></div>",
+								resolution: 'low_resolution',
                 limit: 16
 				
             });
